63198a8c3722ff6fdc8d6c8e20f10f948af6bb61
Ravi Bagin <104773449+rbagin@users.noreply.github.com>
2022-07-05 14:31:48 +0200

    WIP: script.js almost done, HTML progress

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let loc = null
-const APIkey = '7cd8bc6beeea1b8d4ca9f672d18493c9'
-
+const APIkey = "7cd8bc6beeea1b8d4ca9f672d18493c9";
 
 const getCoords = () => {
-    const sucess = (obj) => {
-        loc = {
-            latitude : obj.coords.latitude,
-            longitude : obj.coords.longitude
-        }
-    }
-    navigator.geolocation.getCurrentPosition
-}
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${APIkey}`)
-.then(resp.json())
-.then(forecast => {
-    const weatherTemp = forecast.weather.map((condition) => condition.main)
-    return {
-        temperature: forecast.main.temp,
-        conditions: weatherTemp
-    }
-})
+  const success = (obj) => {
+    const latitude = obj.coords.latitude;
+    const longitude = obj.coords.longitude;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}`
+    )
+      .then((resp) => resp.json())
+      .then((forecast) => {
+        const weatherTemp = forecast.weather.map((condition) => condition.main);
+        return {
+          temperature: forecast.main.temp,
+          conditions: weatherTemp,
+        };
+      })
+      .then((weather) => {
+        const weatherReport = `<div>${weather.temperature}</div>
+    <div>${weather.conditions
+      .map((condition) => `<span>${condition}</span>`)
+      .join(" ")}</div>`;
+        const appElem = document.getElementById("app");
+        appElem.innerHTML = weatherReport;
+      });
+  };
+  navigator.geolocation.getCurrentPosition(success);
+};
