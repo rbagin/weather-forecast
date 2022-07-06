8d19f70e7343c8ac6a33da88d8c41503c1dda1c2
Rohan Nicholls <rohan.nicholls@gmail.com>
2022-07-06 23:30:28 +0200

    added city, css tweaks, weather description

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 const APIkey = "7cd8bc6beeea1b8d4ca9f672d18493c9";
+const geoCodingKey = "pk.cae1fb70be9ed8648746817c82b0e03c";
+
 var kelvin = 293.15;
 var celsius = kelvin - 273.15;
-const getCoords = () => {
-  const success = (obj) => {
+
+const getCoords = async () => {
+  const success = async (obj) => {
     const latitude = obj.coords.latitude;
     const longitude = obj.coords.longitude;
 
-    fetch(
+    const weatherObj = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}`
-    )
-      .then((resp) => resp.json())
-      .then((forecast) => {
-        const weatherConditions = forecast.weather.map((condition) => ({
-          title: condition.main,
-          description: condition.description,
-        }));
-        return {
-          temperature: forecast.main.temp,
-          conditions: weatherConditions,
-        };
-      })
-      .then((weather) => {
-        const dateElem = document.getElementById("date");
-        const tempElem = document.getElementById("temperature");
-        const conditionsElem = document.getElementById("conditions");
-        const temperature = kelvinToCelcius(weather.temperature).toFixed(1);
-        // set the contents
-        tempElem.innerText = `${temperature} ºC`;
-        dateElem.innerText = dateString("sk-SK");
-        debugger;
-        conditionsElem.innerHTML = conditionsString(weather.conditions);
-      });
+    ).then((response) => response.json());
+    const weatherConditions = weatherObj.weather.map((condition) => ({
+      title: condition.main,
+      description: condition.description,
+    }));
+    const weather = {
+      temperature: weatherObj.main.temp,
+      conditions: weatherConditions,
+    };
+
+    const addressObj = await fetch(
+      `https://us1.locationiq.com/v1/reverse?key=${geoCodingKey}&lat=${latitude}&lon=${longitude}&format=json`
+    ).then((response) => response.json());
+    const cityName = addressObj?.address?.city || "Unknown City";
+    console.log("addressJson", addressObj);
+
+    // Elements in page
+    const dateElem = document.getElementById("date");
+    const tempElem = document.getElementById("temperature");
+    const conditionsElem = document.getElementById("conditions");
+    const locationElem = document.getElementById("location");
+
+    // calculated values
+    const temperature = kelvinToCelcius(weather.temperature).toFixed(1);
+    // set the contents
+    tempElem.innerText = `${temperature} ºC`;
+    dateElem.innerText = dateString(whichLanguage());
+    conditionsElem.innerHTML = conditionsString(weather.conditions);
+    locationElem.innerText = cityName;
   };
   navigator.geolocation.getCurrentPosition(success);
 };
@@ -53,11 +62,33 @@ const conditionsString = (conditions) => {
   const str = conditions
     .map(
       (condition) => `
-    <div>
-      <span class="condition-title">${condition.title}</span>
-      <span class="condition-desc">${condition.description}</span>
+    <div class="condition">
+      <div class="condition-title">${condition.title}</div>
+      <div class="condition-desc">${condition.description}</div>
     </div>`
     )
     .join(" ");
   return `<div>${str}</div>`;
+};
+
+const whichLanguage = () => {
+  const languageMap = {
+    slovak: "sk-SK",
+    english: "en-EN",
+    dutch: "nl-NL",
+  };
+  const chosen = document.getElementById("language").value;
+  return languageMap[chosen];
+};
+
+const register = (id, type, handler) => {
+  const elem = document.getElementById(id);
+  if (elem) {
+    elem.addEventListener(type, handler);
+  }
+};
+
+const main = () => {
+  register("language", "change", () => getCoords());
+  getCoords();
 };
